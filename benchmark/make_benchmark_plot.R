fcc5677ed826b8f5e579dfedad2425034fdd1794
AJ Pryor <apryor6@gmail.com>
2017-02-07 13:49:12 -0800

    compiler flags

--- benchmark/make_benchmark_plot.R
+++ benchmark/make_benchmark_plot.R
@@ -1,4 +1,4 @@
-setwd(".")
+setwd("~/Documents/MATLAB/3d-refine/splinter/benchmark/")
 library(ggplot2)
 library(dplyr)
 library(tidyr)
@@ -11,7 +11,7 @@ ggplot(data=df,aes(x=N,y=value, color=time)) +
 f<-ggplot(data=df,aes(x=N^2,y=log(value), color=time)) +
   geom_line(aes(group=time)) + 
   scale_colour_brewer(type = "qual", palette = 6, direction = 1, labels=c("MATLAB interp2","C++ splinterp2")) + 
-  theme(legend.position = "right") + 
+  theme(legend.position ="bottom") + 
   #theme(legend.position = c(.835,.85)) + 
   labs(title="2D to 1D Bilinear Interpolation Benchmark\n(i7-6800k, 12 threads)", y="Log Time (s)", x="N")
 pdf("log_time.pdf")
@@ -21,7 +21,7 @@ dev.off()
 f<-ggplot(data=df,aes(x=N^2,y=(value), color=time)) +
   geom_line(aes(group=time)) + 
   scale_colour_brewer(type = "qual", palette = 6, direction = 1, labels=c("MATLAB interp2","C++ splinterp2")) + 
-  theme(legend.position = "right") + 
+  theme(legend.position = "bottom") + 
   #theme(legend.position = c(.835,.85)) + 
   labs(title="2D to 1D Bilinear Interpolation Benchmark\n(i7-6800k, 12 threads)", y="Time (s)", x="N")
 pdf("linear_time.pdf")
@@ -33,8 +33,8 @@ df$ratioT = df$interp2 / df$splinterp2
 f<-ggplot(data=df,aes(x=N^2,y=(ratioT))) +
   geom_line() +
   scale_colour_brewer(type = "qual", palette = 6, direction = 1, labels=c("MATLAB interp2","C++ splinterp2")) + 
-  theme(legend.position = "right") + 
-  #theme(legend.position = c(.835,.85)) + 
+  theme(legend.position = "bottom") + 
+  #theme(legend.position = "bottom") + 
   labs(title="Relative Speedup", y="Ratio", x="N")
 pdf("ratio.pdf")
 print(f)
