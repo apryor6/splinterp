c2a066c294daa37e1baf28cc46e4c9a716b23282
AJ Pryor <apryor6@gmail.com>
2017-02-07 13:41:34 -0800

    initial version complete

--- benchmark/make_benchmark_plot.R
+++ benchmark/make_benchmark_plot.R
@@ -8,24 +8,34 @@ ggplot(data=df,aes(x=N,y=value, color=time)) +
   geom_line(aes(group=time)) + 
   scale_colour_brewer(type = "qual", palette = 6, direction = 1)
 
-ggplot(data=df,aes(x=N^2,y=log(value), color=time)) +
+f<-ggplot(data=df,aes(x=N^2,y=log(value), color=time)) +
   geom_line(aes(group=time)) + 
   scale_colour_brewer(type = "qual", palette = 6, direction = 1, labels=c("MATLAB interp2","C++ splinterp2")) + 
   theme(legend.position = "right") + 
   #theme(legend.position = c(.835,.85)) + 
   labs(title="2D to 1D Bilinear Interpolation Benchmark\n(i7-6800k, 12 threads)", y="Log Time (s)", x="N")
+pdf("log_time.pdf")
+print(f)
+dev.off()
 
-ggplot(data=df,aes(x=N^2,y=(value), color=time)) +
+f<-ggplot(data=df,aes(x=N^2,y=(value), color=time)) +
   geom_line(aes(group=time)) + 
   scale_colour_brewer(type = "qual", palette = 6, direction = 1, labels=c("MATLAB interp2","C++ splinterp2")) + 
   theme(legend.position = "right") + 
   #theme(legend.position = c(.835,.85)) + 
   labs(title="2D to 1D Bilinear Interpolation Benchmark\n(i7-6800k, 12 threads)", y="Time (s)", x="N")
+pdf("linear_time.pdf")
+print(f)
+dev.off()
 
 df <- read.csv("benchmark_data.csv",col.names=c("N","interp2","splinterp2"))
-df$ratio = df$interp2 / df$splinterp2
-ggplot(data=df,aes(x=N^2,y=(ratio))) +
+df$ratioT = df$interp2 / df$splinterp2
+f<-ggplot(data=df,aes(x=N^2,y=(ratioT))) +
+  geom_line() +
   scale_colour_brewer(type = "qual", palette = 6, direction = 1, labels=c("MATLAB interp2","C++ splinterp2")) + 
   theme(legend.position = "right") + 
   #theme(legend.position = c(.835,.85)) + 
   labs(title="Relative Speedup", y="Ratio", x="N")
+pdf("ratio.pdf")
+print(f)
+dev.off()
--- splinter.h
+++ splinter.h
@@ -12,10 +12,10 @@
 #include "mex.h"
 
 #ifndef NUM_THREADS
-#define NUM_THREADS 300
+#define NUM_THREADS 4
 #endif
 #ifndef SERIAL_LIMIT
-#define SERIAL_LIMIT 2047
+#define SERIAL_LIMIT 4194304
 #endif
 // Interpolate position x,y from 2D array stored in data, which is assumed to lie on an integer coordinate system.
 // Values of x and y that lie outside of the dimensions of data are set to 0.
