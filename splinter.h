a4ac91a13f783d312031eabfc54afb9b2fb6acc8
AJ Pryor <apryor6@gmail.com>
2017-02-07 21:17:12 -0800

    changed serial cutoff limit

--- splinter.h
+++ splinter.h
@@ -12,10 +12,10 @@
 #include "mex.h"
 
 #ifndef NUM_THREADS
-#define NUM_THREADS 8
+#define NUM_THREADS 4
 #endif
 #ifndef SERIAL_LIMIT
-#define SERIAL_LIMIT 4194304
+#define SERIAL_LIMIT 65535
 
 #endif
 // Interpolate position x,y from 2D array stored in data, which is assumed to lie on an integer coordinate system.
